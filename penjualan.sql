7d245c5c7b1e0398a5c0ca5e641a059c7181c142
calondoktor <68120320+vitonugroho@users.noreply.github.com>
2024-07-03 07:24:57 +0700

    Add database backup file

---
+++ penjualan.sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3308
+-- Generation Time: Jul 03, 2024 at 02:22 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `penjualan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `ID_Barang` varchar(10) NOT NULL,
+  `Nama_Barang` varchar(50) DEFAULT NULL,
+  `Jenis` varchar(20) DEFAULT NULL,
+  `Ukuran` varchar(5) DEFAULT NULL,
+  `HargaBeli` decimal(10,0) DEFAULT NULL,
+  `HargaJual` decimal(10,0) DEFAULT NULL,
+  `Stok` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `NoFaktur` varchar(20) NOT NULL,
+  `Tanggal` varchar(20) DEFAULT NULL,
+  `ID_Customer` varchar(10) DEFAULT NULL,
+  `TotalBeli` decimal(10,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `penjualan`
+--
+
+INSERT INTO `penjualan` (`NoFaktur`, `Tanggal`, `ID_Customer`, `TotalBeli`) VALUES
+('TR0001', '03-07-2024', 'CS001', 1200000),
+('TR0002', '03-07-2024', 'CS002', 11200000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualanrinci`
+--
+
+CREATE TABLE `penjualanrinci` (
+  `NoFaktur` varchar(20) DEFAULT NULL,
+  `ID_Barang` varchar(10) DEFAULT NULL,
+  `Nama_Barang` varchar(50) DEFAULT NULL,
+  `Jumlah` int(11) DEFAULT NULL,
+  `Harga` decimal(10,0) DEFAULT NULL,
+  `Total` decimal(10,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `penjualanrinci`
+--
+
+INSERT INTO `penjualanrinci` (`NoFaktur`, `ID_Barang`, `Nama_Barang`, `Jumlah`, `Harga`, `Total`) VALUES
+('TR0001', 'BR001', 'Logitech G Pro X Superlight', 1, 1200000, 1200000),
+('TR0002', 'BR001', 'Baju Koko', 40, 280000, 11200000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`ID_Barang`);
+
+--
+-- Indexes for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`NoFaktur`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
